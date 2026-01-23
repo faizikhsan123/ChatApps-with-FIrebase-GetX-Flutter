17c31311320c126ae6bf53402b9d9728b6c268c4
unknown <faizashary19@gmail.com>
2026-01-23 15:07:38 +0700

    Design Login Page

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
-  const LoginView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: const Color.fromARGB(193, 70, 116, 186),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                height: Get.height * 0.5,
+
+                child: Lottie.asset('assets/lottie/login.json'),
+              ),
+            ),
+            Container(
+              width: Get.width * 0.8,
+              height: Get.height * 0.05,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: Get.height * 0.05,
+                      child: Icon(
+                        Icons.email,
+                        size: 25,
+                        color: const Color.fromARGB(255, 152, 46, 46),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "Login With Google",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: const Color.fromARGB(255, 17, 16, 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
