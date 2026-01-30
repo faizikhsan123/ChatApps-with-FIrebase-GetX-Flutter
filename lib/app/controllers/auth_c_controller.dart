de61703835c277dd10895686ab0707becbd65f05
unknown <faizashary19@gmail.com>
2026-01-30 13:59:07 +0700

    add google to firebase

--- lib/app/controllers/auth_c_controller.dart
+++ lib/app/controllers/auth_c_controller.dart
@@ -1,6 +1,5 @@
-
-
 import 'package:chat_apps/app/routes/app_pages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -10,37 +9,43 @@ class AuthCController extends GetxController {
 
   // _ artinya private di dart
 
-
-  final GoogleSignIn _googleSignIn = GoogleSignIn();//membuat objek GoogleSignIn
+  final GoogleSignIn _googleSignIn = GoogleSignIn(); //membuat objek GoogleSignIn
 
   GoogleSignInAccount? _currentUser; //untuk menyimpan data akun Google user
 
-  Future<void> login() async {    //fungsi untuk login gogle
+  UserCredential? userCredential; //userCredential 
 
+  Future<void> login() async {
     try {
-      // Membuka popup login Google.
-      // Jika user berhasil login, data akun Google (email, nama, foto, dll)
-      // akan disimpan ke dalam variabel _currentUser.
-      await _googleSignIn.signIn().then((value) => _currentUser = value);
+      await _googleSignIn.signOut(); //memastikan agar user logout sebelum login
 
-      await _googleSignIn.isSignedIn().then((value) { //untuk mengecek apakah user sudah login atau belum
-        if (value) { //jika sudah login (value = true)
-        print("Sudah berhasil login");
-        isAuth.value = true;
-        Get.offAllNamed(Routes.HOME);
-        print(_currentUser); //tampilkan data var  _currentUser di debug console
-        
-        } else {
-          print("Belum login");
-        }
-      });
-    } catch (error) {
-      print(error);
+      await _googleSignIn.signIn().then((value) => _currentUser = value,); //menangkap data akun Google ke _currentUser
+
+      final isLogin = await _googleSignIn.isSignedIn(); //untuk mengecek apakah user sudah login atau belum
+
+      if (isLogin) { //islogin bernilai true jika user sudah login
+        print("sudah berhasil login");
+        print(_currentUser); //tampilkan data akun Google user di termnal
+
+        final gooleAuth = await _currentUser!.authentication; //untuk mengambil data autentikasi (credential) dari currentUser
+
+        final credential = GoogleAuthProvider.credential( //untuk membuat credential
+          idToken: gooleAuth.idToken, //idtoken diambil dari data gooleAuth
+          accessToken: gooleAuth.accessToken, //accesstoken diambil dari data gooleAuth
+        );
+
+       await FirebaseAuth.instance.signInWithCredential(credential).then((value) => userCredential = value,); //menangap data credential ke userCredential
+
+       print(userCredential); //tampilkan data credential di terminal
+
+      }
+    } catch (e) {
+      print(e);
     }
   }
 
-  void logout() async { //fungsi untuk logout
-  await _googleSignIn.signOut(); //untuk logout
-    Get.offAllNamed(Routes.LOGIN); 
+  void logout() async {
+    await _googleSignIn.signOut(); //untuk logout
+    Get.offAllNamed(Routes.LOGIN);
   }
 }
