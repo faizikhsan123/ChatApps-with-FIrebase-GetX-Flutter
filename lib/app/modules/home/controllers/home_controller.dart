a1b65ba07c72727c319a66e86eb35b80410afda4
unknown <faizashary19@gmail.com>
2026-02-04 20:43:13 +0700

    nested stream builder for data user

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -3,9 +3,16 @@ import 'package:get/get.dart';
 
 class HomeController extends GetxController {
 
-  Stream<DocumentSnapshot<Map<String, dynamic>>> chatStream(String email) { //function stream 
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-    return firestore.collection("users").doc(email).snapshots(); //ambil dari collection users dengan email tertentu
+  Stream<DocumentSnapshot<Map<String, dynamic>>> chatStream(String email) { //function stream 
+  
+
+    return firestore.collection("users").doc(email).snapshots(); //ambil dari collection users dengan email yang login
+  }
+
+  Stream<DocumentSnapshot<Map<String, dynamic>>> UserStream(email) { //stream untuk user
+    return firestore.collection("users").doc(email).snapshots(); //ambil dari collection users dengan email  teman kita
+
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -56,48 +56,87 @@ class HomeView extends GetView<HomeController> {
             ),
           ),
           SizedBox(height: 10),
-          Expanded(
-            child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-              //stream builder ngambil data realtimne wajib dikasi tipe
-              stream: controller.chatStream(authC.user!.value.email! ), //jalankan stream dan ambil email user yang sedang login
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.active) {
-                  //jika koenksi terhubung
-                  var allChats = (snapshot.data!.data() as Map<String, dynamic>)["chats"]as List;
-                  //ambil isi dari snapshot trs ambil data dari firebase kaarena data dari firebase masi object diubah ke mapping trs ambil key chats dan diubah ke bentuk list kenappa akrena memastikan var allchats adalah list dan bisa dapat length dkk
-                  return ListView.builder(
-                    itemCount: allChats.length,
-                    itemBuilder: (context, index) => ListTile(
-                      onTap: () {
-                        Get.toNamed(Routes.CHAT);
-                      },
-                      leading: CircleAvatar(),
-                      title: Text(
-                        "Orang ke ${index + 1}",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      subtitle: Text(
-                        "Status  ke ${index + 1}",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
+        Expanded(
+          
+  child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+    
+    //stream builder ngambil data realtimne wajib dikasi tipe
+    stream: controller.chatStream(authC.user!.value.email!), 
+    //jalankan stream dan ambil email user yang sedang login
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.active) {
+        //jika koenksi terhubung
 
-                      trailing: Chip(label: Text("3")),
+        var allChats = (snapshot.data!.data() as Map<String, dynamic>)["chats"] as List;
+        //ambil isi dari snapshot trs ambil data dari firebase kaarena data dari firebase masi object
+        //diubah ke mapping trs ambil key chats dan diubah ke bentuk list
+        //kenappa akrena memastikan var allchats adalah list dan bisa dapat length dkk
+
+        return ListView.builder(
+          itemCount: allChats.length,
+          itemBuilder: (context, index) {
+            return StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              //stream builder kedua untuk user
+              stream: controller.UserStream( allChats[index]["connection"]),
+              //stream nya bngambil email  darri connection (email teman kita)
+
+              builder: (context, snapshot2) {
+                if (snapshot2.connectionState == ConnectionState.active) {
+
+                  var allUser = snapshot2.data!.data() as Map<String, dynamic>;
+                  //AMBIL DATA USER DARI SNAPSHOT2 (BUKAN SNAPSHOT)
+
+                  return ListTile(
+                    onTap: () {
+                      Get.toNamed(
+                        Routes.CHAT, 
+                      );
+                    },
+                    leading: allUser["photoUrl"] == null ? CircleAvatar()
+                     : CircleAvatar( //jika photoUrl null maka tampil circleavatar jika tidak tampil gambar
+                      backgroundImage: NetworkImage(
+                        allUser["photoUrl"],
+                      ),
                     ),
-                    padding: EdgeInsets.zero,
+                    title: Text(
+                      allUser["name"] ?? "No Name", //ambil dari stream kedua
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    subtitle: Text(
+                      allUser["status"] ?? "", //ambil dari stream kedua
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    trailing: allChats[index]["total_unread"] == 0 ? null : Chip( //jika total_unread 0 maka null
+                      label: Text(
+                        allChats[index]["total_unread"].toString(), //ambil dari stream pertama
+                      ),
+                    ),
+                    
                   );
+                  
                 }
                 return Center(
                   child: CircularProgressIndicator(),
-                ); //jika tidak terhubung
+                );
               },
-            ),
-          ),
+            );
+          },
+        );
+      }
+
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    },
+  ),
+),
+
         ],
       ),
       floatingActionButton: FloatingActionButton(
