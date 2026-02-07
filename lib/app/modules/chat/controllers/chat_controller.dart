57d5cced5a917e7f5a591690a482710985f6f227
unknown <faizashary19@gmail.com>
2026-02-07 22:36:54 +0700

    fixing logic total unread

--- lib/app/modules/chat/controllers/chat_controller.dart
+++ lib/app/modules/chat/controllers/chat_controller.dart
@@ -12,7 +12,7 @@ class ChatController extends GetxController {
 
   late TextEditingController chatC;
 
-  int total_unread =0; //var total_unreead = 0;
+  int total_unread = 0;
 
   void addEmojiToChat(Emoji emoji) {
     chatC.text = chatC.text + emoji.emoji;
@@ -43,49 +43,50 @@ class ChatController extends GetxController {
     super.dispose();
   }
 
-void newChat(String email, String chatId, String friendEmail, String chat) async {
-  CollectionReference chats = firestore.collection("chats");
-  CollectionReference users = firestore.collection("users"); //tambaahkan collection users
+  void newChat(
+    String email,
+    String chatId,
+    String friendEmail,
+    String chat,
+  ) async {
+    CollectionReference chats = firestore.collection("chats");
+    CollectionReference users = firestore.collection("users");
 
-  String date = DateTime.now().toIso8601String();
+    String date = DateTime.now().toIso8601String();
 
-  //ambil document berdasarkan chat id lalu di chat id itu bikin sub collection chats dan tambahkan data
-  final newChat = await chats.doc(chatId).collection("chats").add({
-    "pengirim": email, //email user yg login
-    "penerima": friendEmail, // dari parameter
-    "pesan": chat,//dari textfield
-    "time": date,
-    "isRead": false,
-  });
+    final newChat = await chats.doc(chatId).collection("chats").add({
+      "pengirim": email,
+      "penerima": friendEmail,
+      "pesan": chat,
+      "time": date,
+      "isRead": false,
+    });
 
-  await users.doc(email).collection("chats").doc(chatId).update( //masuk ke collection users di subcollection chats milik user yg login 
-    { //trs update field tertentu
+    await users.doc(email).collection("chats").doc(chatId).update({
       "lastTime": date,
+    });
+
+    final cekChatFriends = await users
+        .doc(friendEmail)
+        .collection("chats")
+        .doc(chatId)
+        .get();
+
+    if (cekChatFriends.exists) {
+      int currentUnread = cekChatFriends.data()?["total_unread"] ?? 0;
+
+      await users.doc(friendEmail).collection("chats").doc(chatId).update({
+        "lastTime": date,
+        "total_unread": currentUnread + 1,
+      });
+    } else {
+      await users.doc(friendEmail).collection("chats").doc(chatId).set({
+        "connection": email,
+        "lastTime": date,
+        "total_unread": 1,
+      });
     }
-  ); 
 
-  final cekChatFriends = await users.doc(friendEmail).collection("chats").doc(chatId).get(); //ini untuk cek chat milik teman kita
-
-  if (cekChatFriends.exists) { //jika chat milik teman kita sudah ada maka update lastTime
-    int currentUnread = cekChatFriends.data()?["total_unread"] ?? 0;
-
-    await users.doc(friendEmail).collection("chats").doc(chatId).update({
-      "lastTime": date,
-      "total_unread": currentUnread + 1,
-    });
-  } else { //jika chat milik teman kita belum ada maka tambahkan data
-    await users.doc(friendEmail).collection("chats").doc(chatId).set({
-      "connection": email,
-      "lastTime": date,
-      "total_unread": 1,
-    });
+    await chats.doc(chatId).update({"lastTime": date});
   }
-
-  //update lastTime
-  await chats.doc(chatId).update({
-    "lastTime": date,
-  });
-}
-
-
 }
